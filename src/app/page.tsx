d7bd28722a48838d35154cfd036db46d10898060
Tuan Nguyen <ntuan.2502@gmail.com>
2025-02-06 17:00:10 +0700

    feat(ui): add Typewriter effect to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+import Typewriter from "@/cuicui/other/text-animation/typewritter/typewritter";
+
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen font-bold text-3xl">
-      Hello world!
+    <div className="text-xl flex items-center justify-center h-screen px-2">
+      <p className="whitespace-pre-wrap">
+        <span>{"We're born 🌞 to "}</span>
+        <Typewriter
+          text={[
+            "experience",
+            "dance",
+            "love",
+            "be alive",
+            "create things that make the world a better place",
+          ]}
+          speed={70}
+          className="text-yellow-500"
+          waitTime={1500}
+          deleteSpeed={40}
+          cursorChar={"_"}
+        />
+      </p>
     </div>
   );
 }
