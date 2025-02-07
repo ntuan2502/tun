0e6a6c653bbea098c00fb33cc0aed046ace3b8ba
Tuan Nguyen <ntuan.2502@gmail.com>
2025-02-07 09:28:43 +0700

    feat(ui): adjust theme switch button

--- src/app/components/ThemeSwitcher.tsx
+++ src/app/components/ThemeSwitcher.tsx
@@ -15,26 +15,24 @@ export default function ThemeSwitcher() {
   if (!mounted) return null; // Don't render anything until mounted
 
   return (
-    <div className="transition-all duration-500 ease-in-out">
-      <div className="flex space-x-6">
-        {/* Render Light Mode button only if the current theme is dark */}
-        {theme === "dark" ? (
-          <button
-            onClick={() => setTheme("light")}
-            className="p-3 bg-blue-500 text-white rounded-full transition-transform transform hover:scale-105 hover:bg-blue-600"
-          >
-            <SunIcon className="h-6 w-6" />
-          </button>
-        ) : (
-          // Render Dark Mode button only if the current theme is light
-          <button
-            onClick={() => setTheme("dark")}
-            className="p-3 bg-gray-800 text-white rounded-full transition-transform transform hover:scale-105 hover:bg-gray-700"
-          >
-            <MoonIcon className="h-6 w-6" />
-          </button>
-        )}
-      </div>
+    <div className="flex space-x-6">
+      {/* Render Light Mode button only if the current theme is dark */}
+      {theme === "dark" ? (
+        <button
+          onClick={() => setTheme("light")}
+          className="p-3 text-white rounded-full transition-transform transform hover:scale-125"
+        >
+          <SunIcon className="h-6 w-6" />
+        </button>
+      ) : (
+        // Render Dark Mode button only if the current theme is light
+        <button
+          onClick={() => setTheme("dark")}
+          className="p-3 text-dark rounded-full transition-transform transform hover:scale-125"
+        >
+          <MoonIcon className="h-6 w-6" />
+        </button>
+      )}
     </div>
   );
 }
